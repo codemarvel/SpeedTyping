f5693316929a7ec8b492e5c198e246862a4458f2
Jatin Rajput <jatinraj1999007@gmail.com>
2020-04-07 19:41:03 +0530

    backspace prob. & errors count

--- typing.js
+++ typing.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
     let text = $('#text').text(); //Text to be typed
-    let index = 0;
+    $('#inputText').val('');
+    let index = 0, errors = 0;
     console.log(text);
     $('#inputText').keypress(function(){
+        index = $('#inputText').val().length;
         if(String.fromCharCode(event.which) === text[index]){
             $('#errorText').empty();
-            index++;
         }
         else{
             $('#errorText').html("<span style='color:tomato'>You mistyped</span>");
+            errors++;
+            console.log('error occured');
         }
         if(index == text.length){
-            $('#errorText').html("<span style='color:blue'>Completed</span>");
-            $(this).offkeypress();
+            $('#errorText').html(`<span style='color:blue'>Completed</span><span style='color:tomato'> Errors : ${errors}</span>` );
+            $(this).off();
         }
     });
 });
